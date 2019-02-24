1a08d3d279444ec77a7cf53c55f9306540c9a2f1
vnepveu <victor.nepveu@imt-atlantique.net>
2019-02-24 22:24:16 +0100

    Add text tokenization

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import csv_to_pickle as csv
-import text_processing
+import text_processing, document_io
 import os
 
+# TODO : have the program ask for input
 #  The csv file contains texts separate by commas
 csv_filename = "enter filename with .csv"
 base_name = "enter pickle name without extension"
@@ -11,14 +12,14 @@ pickle_name = base_name + ".pkl"
 
 # If the pickle file exists, then go directly to treatment
 cwd = os.getcwd()
-complete_path = "{}/{}".format(cwd, pickle_name)
-if not os.path.exists(complete_path):
+if not os.path.exists("{}/{}".format(cwd, pickle_name)):
     print("Pickle not found under {}, treating the csv".format(complete_path))
     csv_path = "{}/{}".format(cwd, csv_filename)
     file_dictionary = csv.open_csv(csv_path)
     csv.save_csv_as_pickle(file_dictionary, pickle_name)
 else:
     print("Pickle found, not treating the CSV")
+    file_dictionary = document_io.load(pickle_name)
 
 """ Process text : 
  - tokenization
@@ -29,5 +30,16 @@ else:
 # TODO : handle the choice of language, defaulting to english for now
 
 # If the treated text exists, do not run the treatment again
-treated_dictionary_pickle_name = pic
-treated_dictionary =
+treated_dictionary_pickle_name = base_name + "_treated_dict" + ".pkl"
+
+if not os.path.exists("{}/{}".format(cwd, treated_dictionary_pickle_name)):
+    #  Specify the parameters, cf text_processing.py for explanation
+    treated_dictionary = text_processing.treat_dictionary(file_dictionary, number_of_texts=1000000, min_len=1,
+                                                          max_len=15
+                                                          )
+    document_io.save(treated_dictionary_pickle_name, treated_dictionary, cwd)
+    print("The text was treated and saved under {}{}".format(cwd, treated_dictionary_pickle_name))
+else:
+    print("Treated text was found, not processing it")
+    treated_dictionary = document_io.load(treated_dictionary_pickle_name)
+
